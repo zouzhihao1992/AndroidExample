e17f17358bf01f559aa2fe5f20626ac24e04d4d9
zouzhihao1992 <zouzhihao_1992@163.com>
2016-02-15 21:41:03 +0800

    add RecycleView listview(simple)

---
+++ app/src/main/java/com/android/zzh/androidexample/activity/RecyclerViewListView.java
@@ -0,0 +1,7 @@
+package com.android.zzh.androidexample.activity;
+
+/**
+ * Created by zzh on 16/2/15.
+ */
+public class RecyclerViewListView {
+}
---
+++ app/src/main/java/com/android/zzh/androidexample/adapter/MySimpleAdapter.java
@@ -0,0 +1,7 @@
+package com.android.zzh.androidexample.adapter;
+
+/**
+ * Created by zzh on 16/2/15.
+ */
+public class MySimpleAdapter {
+}
